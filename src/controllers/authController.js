88cc4f18016c1fba8fa0d97260c282f251fad6b8
Romulo <romulofreires1@gmail.com>
2019-04-11 12:44:58 -0300

    Impedindo de criar usuários já cadastrados

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,11 +5,18 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const {email} = req.body;
+
     try {
+        if(await User.findOne({email})){
+            return res.status(400).send({error: 'User already exists'});
+        }
+
         const user =  await User.create(req.body);
+
         return res.send({user});
     }catch (err) {
-        return res.status(400).send({error: User});
+        return res.status(400).send({error: 'Registration Failed'});
     }
 });
 
